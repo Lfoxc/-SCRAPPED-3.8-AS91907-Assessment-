d4cc7aef93444f5461f66f646346a0c64195bcab
Lfoxc <18lbeale@wakatipu.school.nz>
2022-07-11 20:50:03 +1200

    This commit includes: x1 Manifest JS file x1 HTML page (Includes x2 buttons with basic functionality) x1 CSS page x1 Content JS x1 Background JS (Empty)

---
+++ STUDYspace browser extension/content.js
@@ -0,0 +1,11 @@
+const manualbtn = document.getElementById("manbtn");
+
+const pomodorobtn = document.getElementById("pombtn");
+
+manualbtn.addEventListener("click", function() {
+  alert("Manual Mode selected!");
+});
+
+pomodorobtn.addEventListener("click", function() {
+  alert("Pomodoro Mode selected!");
+})
