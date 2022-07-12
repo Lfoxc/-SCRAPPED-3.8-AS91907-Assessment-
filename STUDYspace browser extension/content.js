d845dab6ae0b38ca258054c1728db03d2e3a6230
Lfoxc <18lbeale@wakatipu.school.nz>
2022-07-12 22:22:03 +1200

    This commit includes x2 new webpages.
    The first is a site selection page.
    
    The second is a timer selection page.
    
    I also emptied the content.js file because I do not actually need it 
    yet.

--- STUDYspace browser extension/content.js
+++ STUDYspace browser extension/content.js
@@ -1,11 +1,1 @@
-const manualbtn = document.getElementById("manbtn");
 
-const pomodorobtn = document.getElementById("pombtn");
-
-manualbtn.addEventListener("click", function() {
-  alert("Manual Mode selected!");
-});
-
-pomodorobtn.addEventListener("click", function() {
-  alert("Pomodoro Mode selected!");
-})
